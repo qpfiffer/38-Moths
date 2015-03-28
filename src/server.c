fb3c08e088f79c0e6afb2d602f02651d0b09b975
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-03-28 11:17:26 -0700

    Log with worker identity.

--- src/server.c
+++ src/server.c
@@ -38,6 +38,7 @@ typedef struct acceptor_arg {
 typedef struct worker_arg {
 	const route *all_routes;
 	const size_t num_routes;
+	const int worker_ident;
 } worker_arg;
 
 static void *acceptor(void *arg) {
@@ -54,7 +55,7 @@ static void *acceptor(void *arg) {
 
 	accepted_socket_queue = mq_open(ACCEPTED_SOCKET_QUEUE_NAME, O_WRONLY | O_CREAT, 0644, &asq_attr);
 	if (accepted_socket_queue == -1) {
-		log_msg(LOG_ERR, "Could not open accepted_socket_queue.");
+		log_msg(LOG_ERR, "Acceptor: Could not open accepted_socket_queue.");
 		perror("Acceptor: ");
 		return NULL;
 	}
@@ -66,14 +67,14 @@ static void *acceptor(void *arg) {
 		int new_fd = accept(main_sock_fd, (struct sockaddr *)&their_addr, &sin_size);
 
 		if (new_fd == -1) {
-			log_msg(LOG_ERR, "Could not accept new connection.");
+			log_msg(LOG_ERR, "Acceptor: Could not accept new connection.");
 			return NULL;
 		}
 
 		ssize_t msg_size = mq_send(accepted_socket_queue, (char *)&new_fd, sizeof(int), 0);
 		if (msg_size == -1) {
 			/* We want to continue from this error. Maybe the queue is full or something. */
-			log_msg(LOG_ERR, "Could not queue new accepted connection.");
+			log_msg(LOG_ERR, "Acceptor: Could not queue new accepted connection.");
 			close(new_fd);
 		}
 	}
@@ -86,10 +87,11 @@ static void *worker(void *arg) {
 	const worker_arg *args = arg;
 	const size_t num_routes = args->num_routes;
 	const route *all_routes = args->all_routes;
+	const int worker_ident = args->worker_ident;
 
 	accepted_socket_queue = mq_open(ACCEPTED_SOCKET_QUEUE_NAME, O_RDONLY);
 	if (accepted_socket_queue == -1) {
-		log_msg(LOG_ERR, "Could not open accepted_socket_queue.");
+		log_msg(LOG_ERR, "Worker %i: Could not open accepted_socket_queue.", worker_ident);
 		perror("Worker: ");
 		return NULL;
 	}
@@ -99,10 +101,10 @@ static void *worker(void *arg) {
 		ssize_t msg_size = mq_receive(accepted_socket_queue, (char *)&new_fd, sizeof(int), NULL);
 
 		if (msg_size == -1) {
-			log_msg(LOG_ERR, "Could not read from accepted_socket_queue.");
+			log_msg(LOG_ERR, "Worker %i: Could not read from accepted_socket_queue.", worker_ident);
 			break;
 		} else if (new_fd == -1) {
-			log_msg(LOG_ERR, "Got bogus FD from accepted_socket_queue.");
+			log_msg(LOG_ERR, "Worker %i: Got bogus FD from accepted_socket_queue.", worker_ident);
 			break;
 		} else {
 			respond(new_fd, all_routes, num_routes);
@@ -166,6 +168,7 @@ int http_serve(int main_sock_fd,
 		struct worker_arg args = {
 			.all_routes = routes,
 			.num_routes = num_routes,
+			.worker_ident = i
 		};
 		if (pthread_create(&workers[i], NULL, worker, &args) != 0) {
 			goto error;
