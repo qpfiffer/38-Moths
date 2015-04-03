3a9f40020c4fb5710398b9a35285ae14ad5ec75f
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-04-02 18:54:07 -0700

    More stable parsing of range query.

--- src/parse.c
+++ src/parse.c
@@ -1,5 +1,7 @@
 // vim: noet ts=4 sw=4
 #define _GNU_SOURCE
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -42,7 +44,17 @@ range_header parse_range_header(const char *range_query) {
 		return rng;
 	}
 
-	const size_t first_num = strtol(actual_value, NULL, 10);
+	long int _fn = strtol(actual_value, NULL, 10);
+	if ((_fn == LONG_MIN || _fn == LONG_MAX) && errno == ERANGE) {
+		log_msg(LOG_WARN, "Malformed range query, could not parse first_num: %s", range_query);
+		range_header rng = {
+			.limit = 0,
+			.offset = 0
+		};
+		return rng;
+	}
+
+	const size_t first_num = (size_t)_fn;
 	if (i == (strlen(actual_value) - 1)) {
 		/* No second number, just return 0. */
 		range_header rng = {
@@ -55,7 +67,16 @@ range_header parse_range_header(const char *range_query) {
 	/* Otherwise, assume the rest of the value is the second number.
 	 * The '+1' here is for the '-' between the two numbers.
 	 */
-	const size_t second_num = strtol(actual_value + (i + 1), NULL, 10);
+	const long int _sn = strtol(actual_value + (i + 1), NULL, 10);
+	if ((_sn == LONG_MIN || _sn == LONG_MAX) && errno == ERANGE) {
+		/* No second number, just return 0. */
+		range_header rng = {
+			.limit = 0,
+			.offset = first_num
+		};
+		return rng;
+	}
+	const size_t second_num = _sn;
 
 	if (second_num < first_num) {
 		log_msg(LOG_WARN, "Malformed range query, limit smaller than offset: %s", range_query);
