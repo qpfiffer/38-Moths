4242eaba9e238f2e357f6af6b801ac048cbcccae
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-07-04 12:28:02 -0700

    Fix segfaults due to weird GRESHUNKEL structs.

--- python/greshunkel.py
+++ python/greshunkel.py
@@ -1,16 +1,39 @@
-from ctypes import cdll, c_char_p, c_size_t, c_void_p
+from ctypes import cdll, c_char_p, c_size_t, c_void_p, Union,\
+                   LittleEndianStructure, c_char, c_uint8, POINTER,\
+                   c_int, byref
+
+#typedef union greshunkel_var {
+#	const unsigned int fuck_gcc : 1; /* This tricks GCC into doing smart things. Not used. */
+#	char str[MAX_GSHKL_STR_SIZE + 1];
+#	vector *arr;
+#	const greshunkel_ctext *sub_ctext;
+#} greshunkel_var;
+
+class fuck_gcc(LittleEndianStructure):
+    _fields_ = [("fuck_gcc", c_uint8, 1)]
+
+class greshunkel_var(Union):
+    _fields_ = [
+            ("fuck_gcc", fuck_gcc),
+            ("str", c_char * 513),
+            ("arr", c_void_p),
+            ("sub_ctext", c_void_p)
+            ]
 
 lib38moths = cdll.LoadLibrary("lib38moths.so")
 
 lib38moths.gshkl_init_context.restype = c_void_p
-lib38moths.gshkl_add_array.restype = c_void_p
+lib38moths.gshkl_add_array.argtypes = [c_void_p, c_char_p]
+lib38moths.gshkl_add_array.restype = greshunkel_var
+lib38moths.gshkl_add_string_to_loop.argtypes = [POINTER(greshunkel_var), c_char_p]
+lib38moths.gshkl_add_int_to_loop.argtypes = [POINTER(greshunkel_var), c_int]
 lib38moths.gshkl_render.restype = c_char_p
 
 def _add_item_to_greshunkel_loop(loop, value):
     if isinstance(value, str):
-        lib38moths.gshkl_add_string_to_loop(loop, c_char_p(value.encode()))
+        lib38moths.gshkl_add_string_to_loop(byref(loop), c_char_p(value.encode()))
     elif isinstance(value, int):
-        lib38moths.gshkl_add_int_to_loop(loop, c_size_t(value))
+        lib38moths.gshkl_add_int_to_loop(byref(loop), value)
     elif isinstance(value, list):
         raise Exception("Cannot add loops to loops right now. Use subcontexts.")
     elif isinstance(value, dict):
@@ -22,7 +45,8 @@ def _add_item_to_greshunkel_context(ctext, key, value):
     elif isinstance(value, int):
         lib38moths.gshkl_add_int(ctext, c_char_p(key.encode()), c_size_t(value))
     elif isinstance(value, list):
-        new_loop = lib38moths.gshkl_add_array(ctext, c_char_p(key.encode()))
+        converted_key = c_char_p(key.encode())
+        new_loop = lib38moths.gshkl_add_array(ctext, converted_key)
         for subitem in value:
             _add_item_to_greshunkel_loop(new_loop, subitem)
     elif isinstance(value, dict):
