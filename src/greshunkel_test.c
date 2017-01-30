a5e6032e054baaae0f4cfa4b69eb42292993b043
Quinlan Pfiffer <qpfiffer@gmail.com>
2017-01-30 10:24:47 -0800

    I think this is what I want

--- src/greshunkel_test.c
+++ src/greshunkel_test.c
@@ -23,11 +23,11 @@ const char document[] =
 "		</ul>\n"
 "		<p>Context Interpolation:</p>\n"
 "		<p>xXx @sub.name xXx - xXx @sub.other xXx</p>\n"
-"		xXx UNLESS @sub.name xXx\n"
+"		xXx UNLESS NOT @sub.name xXx\n"
 "		<p>You should not see this. Ctext interp.</p>\n"
 "		xXx ENDLESS xXx\n"
 
-"		xXx UNLESS NOT @sub.name xXx\n"
+"		xXx UNLESS @sub.name xXx\n"
 "		<p>You should see this. ctext interp.</p>\n"
 "		xXx ENDLESS xXx\n"
 "		<p>XxX return_hello doesnt_matter_at_all XxX</p>\n"
