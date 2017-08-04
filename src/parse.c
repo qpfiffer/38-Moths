db9f0e5c462fff7b886d0e7f90a17de31221aeca
Quinlan Pfiffer <qpfiffer@gmail.com>
2017-08-04 14:44:35 -0700

    Remove redefintion

--- src/parse.c
+++ src/parse.c
@@ -12,28 +12,7 @@
 #include "grengine.h"
 #include "parse.h"
 #include "logging.h"
-
-/* Pulled from here: http://stackoverflow.com/a/25705264 */
-static char *strnstr(const char *haystack, const char *needle, size_t len) {
-	int i;
-	size_t needle_len;
-
-	/* segfault here if needle is not NULL terminated */
-	if (0 == (needle_len = strlen(needle)))
-		return (char *)haystack;
-
-	/* Limit the search if haystack is shorter than 'len' */
-	len = strnlen(haystack, len);
-
-	for (i=0; i<(int)(len-needle_len); i++)
-	{
-		if ((haystack[0] == needle[0]) && (0 == strncmp(haystack, needle, needle_len)))
-			return (char *)haystack;
-
-		haystack++;
-	}
-	return NULL;
-}
+#include "utils.h"
 
 range_header parse_range_header(const char *range_query) {
 	/* bytes=0- */
